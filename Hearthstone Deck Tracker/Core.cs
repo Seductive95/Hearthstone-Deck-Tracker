a3d88075fa1eacd7abd7d8bed41e2c201f9ba1bd
Alexander Zeier <epikz37@gmail.com>
2016-05-22 10:31:30 +0200

    enable TLS 1.2

--- Hearthstone Deck Tracker/Core.cs
+++ Hearthstone Deck Tracker/Core.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
 using System.IO;
+using System.Net;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
